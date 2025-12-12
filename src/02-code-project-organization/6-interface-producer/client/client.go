5be919e0bb992b6c5a524a4cab09f6e120bab420
prometheus <tihopadaetsneg@gmail.com>
2025-12-13 00:45:37 +0300

    5-10 done, #9 skip

--- src/02-code-project-organization/6-interface-producer/client/client.go
+++ src/02-code-project-organization/6-interface-producer/client/client.go
@@ -2,6 +2,12 @@ package client
 
 import "github.com/teivah/100-go-mistakes/src/02-code-project-organization/6-interface-producer/store"
 
+/*
+	Вот пример создания интерфейса на стороне потребителя. 
+	Я знаю что мне нужно только получение и я реализую это получение. 
+	Даже несмотря на то что у меня реализация на которую я делаю интерфейс лежит в соседнем  пакете - я все равно
+	должен его импортировать в этот пакет и поэтому всё таки стоит даже в таких мелочах следовать этому правилу.
+*/
 type customersGetter interface {
 	GetAllCustomers() ([]store.Customer, error)
 }
